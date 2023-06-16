361ee3772d8faf228a87bcba724bb5b591c4c906
zenM49 <58347499+maarao@users.noreply.github.com>
2023-06-16 19:37:16 -0400

    Created a Projectile class

---
+++ Source/ToonTanks2/Projectile.h
@@ -0,0 +1,30 @@
+// Copyright maarao 2023
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Projectile.generated.h"
+
+UCLASS()
+class TOONTANKS2_API AProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AProjectile();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "Combat")
+	UStaticMeshComponent* StaticMeshComponent;
+
+};
